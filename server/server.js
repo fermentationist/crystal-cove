78072c885624516f6ffafb563b55a73e4be3db47
Dennis Hodges <fermentationist@gmail.com>
2022-12-10 13:34:27 -0600

    Updated package.json scripts

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ import path, { dirname } from "path";
 import apiRouter from "./api/router.js";
 import { API_SERVER_PORT } from "../vite.config.js";
 
-console.log("process.env.PROD_MODE:", process.env.PROD_MODE);
 const PROD_MODE = process.env.PROD_MODE === "true";
 const STATIC_FOLDER = PROD_MODE ? "../build/" : "../";
 const filename = fileURLToPath(import.meta.url);
@@ -17,23 +16,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const staticPath = path.join(dirName, STATIC_FOLDER);
-console.log("staticPath:", staticPath);
-// app.use("*", (req, res, next) => {
-//   console.log("req:", req.url);
-//   return next();
-// })
 app.use(express.static(staticPath));
 
 app.use("/api", apiRouter);
-
-// app.get("/*", (req, res, next) => {
-//   // catch-all route
-//   console.log("PROD_MODE:", PROD_MODE);
-//   const clientPath = PROD_MODE ? "../build/index.html" : "../index.html";
-//   const newPath = path.join(dirName, clientPath);
-//   console.log("newPath:", newPath);
-//   res.sendFile(newPath);
-// });
 
 app.listen(API_SERVER_PORT, () => {
   console.log(`Express app listening on port ${API_SERVER_PORT}`);
