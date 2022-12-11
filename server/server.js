247607f0c633dbfd03d131aab9cb07431e87dee5
Dennis Hodges <fermentationist@gmail.com>
2022-12-11 09:34:57 -0600

    Added node-fetch package, because version of Node on server is older and has no fetch

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import fetch from "node-fetch";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
 import apiRouter from "./api/router.js";
@@ -23,13 +24,13 @@ app.use("/api", apiRouter);
 
 app.listen(API_SERVER_PORT, () => {
   console.log(`Express app listening on port ${API_SERVER_PORT}`);
+  
   setInterval(() => {
     // keep server awake
+    console.log(`Fetching ${SELF_URL} to wake up server`)
     fetch(SELF_URL)
       .then(() =>
-        console.log(
-          `Fetching ${SELF_URL} to wake up server`
-        )
+        console.log("Server is awake")
       )
       .catch((error) => console.log(`Error fetching ${SELF_URL}: ${error.message}`));
   }, 1000 * 60 * 14);
