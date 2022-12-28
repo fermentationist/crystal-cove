a2dcf9f7c632b3007f60554b59384f54bb5cddb6
Dennis Hodges <fermentationist@gmail.com>
2022-12-28 12:51:39 -0500

    Added woke-dyno, set to nap from 6pm to 8am

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import express from "express";
-import fetch from "node-fetch";
+import wakeDyno from "woke-dyno";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
 import apiRouter from "./api/router.js";
@@ -24,16 +24,16 @@ app.use("/api", apiRouter);
 
 app.listen(API_SERVER_PORT, () => {
   console.log(`Express app listening on port ${API_SERVER_PORT}`);
-  
-  setInterval(() => {
-    // keep server awake
-    console.log(`Fetching ${SELF_URL} to wake up server`)
-    fetch(SELF_URL)
-      .then(() =>
-        console.log("Server is awake")
-      )
-      .catch((error) => console.log(`Error fetching ${SELF_URL}: ${error.message}`));
-  }, 1000 * 60 * 14);
+  const offset = 8; // LA
+  const getOffsetHours = hours => (hours + offset) > 24 ? 24 - (hours + offset) : hours + offset;
+  const napStartHour = getOffsetHours(18);
+  const napEndHour = getOffsetHours(8)
+  wakeDyno({
+    url: SELF_URL,
+    interval: 1000 * 60 * 14, // 14 minutes 
+    startNap: [napStartHour, 0, 0, 0],
+    endNap: [napEndHour, 0, 0, 0]
+  }).start();
 });
 
 
